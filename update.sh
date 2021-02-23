9a3c3312d8fbaede40a31f913002d441327222e5
madridi91 <mohamed-amine.dridi@camptocamp.com>
2021-02-23 15:57:38 +0100

    No build when a source image is removed

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-# get the list of tags from a repo - usage: get_tags <token> <repo>
+# Get the list of tags from a repo - usage: get_tags <token> <repo>
 get_tags () {
         local i=1
         local tags=''
@@ -16,16 +16,16 @@ get_tags () {
         echo $tags
 }
 
-# get token
+# Get token
 token=$(curl -s -H 'Content-Type: application/json' -X POST -d '{"username": "'$uname'", "password": "'$upass'"}' https://hub.docker.com/v2/users/login/ | jq -r .token)
 
-# get list of new tags
-new_tags=$(echo $(get_tags $token 'hashicorp/terraform') $(get_tags $token $dest_repo) | tr ' ' '\n' | sort -V | uniq -u)
+# Get list of new tags
+new_tags=$(echo $(get_tags $token 'hashicorp/terraform') $(get_tags $token $dest_repo) $(get_tags $token $dest_repo) | tr ' ' '\n' | sort -V | uniq -u)
 
-# uncomment this if tags prior to 0.13.0 must be ignored - first sync
+# Comment the following line to get version prior to 0.13.0
 new_tags=$(echo ${new_tags/0.12.30//} | cut -d/ -f2)
 
-# build new images only if there are new tags
+# Build new images only if there are new tags
 if [ -n "$new_tags" ]
 then
         for j in $new_tags
